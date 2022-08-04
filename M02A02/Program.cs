c5f3a2aec597b58894c08cc1c797de344124b51f
Fabio Sabariego Rodrigues <fabiosabariego87@hotmail.com>
2022-08-04 10:03:22 -0300

    Adicionar arquivos de projeto.

---
+++ M02A02/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M02A02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         // Exemplo de operadores aritiméticos]
+
+            int op1 = 5;
+            int op2 = 2;
+
+            Console.WriteLine($"Calculando +{op1} = {+op1}");
+            Console.WriteLine($"Calculando -{op1} = {-op1}");
+            Console.WriteLine($"Calculando {op1} + {op2} = {op1 + op2}");
+            Console.WriteLine($"Calculando {op1} * {op2} = {op1 * op2}");
+            Console.WriteLine($"Calculando {op1} / {op2} = {op1 / op2}");
+            Console.WriteLine($"Calculando {op1} resto {op2} = {op1 % op2}");
+            Console.ReadKey();
+        }
+    }
+}
