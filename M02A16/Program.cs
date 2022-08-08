87f3d9de41d3f76fb4a8257a2f937730ea1f1160
Fabio Sabariego Rodrigues <fabiosabariego87@hotmail.com>
2022-08-08 19:14:12 -0300

    Aula de Bitwise
    Deslocamento Right Shift e Left Shift

--- M02A16/Program.cs
+++ M02A16/Program.cs
@@ -10,6 +10,18 @@ namespace M02A16
     {
         static void Main(string[] args)
         {
+            // Trabalhando com operadores de Deslocamento
+            // Right Shift >>
+            // Left Shift <<
+
+            int binario = 0b_01011010;
+            Console.WriteLine($"Valor inicial: {Convert.ToString(binario, toBase: 2)}");
+            int desl1 = binario >> 1;
+            Console.WriteLine($"direita: {Convert.ToString(desl1, toBase: 2)}");
+            int desl2 = binario << 1;
+            Console.WriteLine($"Esquerda: {Convert.ToString(desl2, toBase: 2)}");
+            Console.ReadKey();
+
         }
     }
 }
